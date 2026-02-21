403bef7ca2819f27306184d9861473759de05147
javascript <almagueralexander839@gmail.com>
2026-02-21 14:08:28 -0400

    07

---
+++ assets/js/07-alert.js
@@ -0,0 +1,5 @@
+"use strict";
+var n1 = parseInt(prompt("introduce un numero 1", 0));
+var n2 = parseInt(prompt("introduce un numero 2", 0));
+console.log(n1, n2);
+console.log("la suma es " + (n1 + n2));
